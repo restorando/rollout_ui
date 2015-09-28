020806dbcc0caae6923b0a04fe9341dfc42661fb
Carlos Ribeiro <mail+github@carlosribeiro.me>
2015-09-28 14:20:24 -0300

    Rescue when RolloutUI rails auth is not configured

--- lib/rollout_ui/engine/app/controllers/rollout_ui/application_controller.rb
+++ lib/rollout_ui/engine/app/controllers/rollout_ui/application_controller.rb
@@ -1,6 +1,8 @@
+require 'pry'
 module RolloutUi
   class ApplicationController < ActionController::Base
-    user, password = RolloutUi.config.rails_user, RolloutUi.config.rails_password
+    user      = RolloutUi.config.rails_user rescue nil
+    password  = RolloutUi.config.rails_password rescue nil
     if user && password
       http_basic_authenticate_with name: user, password: password
     end
